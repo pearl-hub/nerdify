8081368a3afe8698f1277355ce207299768ad4c0
Filippo Squillace <squillace.filippo@gmail.com>
2019-03-24 18:12:48 +1100

    Add configuration for powerline

--- pearl-config/install.sh
+++ pearl-config/install.sh
@@ -9,6 +9,15 @@ function post_install(){
     mkdir -p "${PEARL_PKGVARDIR}/plugins/pack/pearl/start"
     install_or_update_git_repo $giturl "${PEARL_PKGVARDIR}/plugins/pack/pearl/start/$pluginname" master
 
+    info "Setting powerline configurations..."
+    warn "This will override the content in ~/.config/powerline"
+    if ask "Do you want to proceed?" "Y"
+    then
+        [[ -e $HOME/.config/powerline ]] && rm -rf "$HOME/.config/powerline"
+        cp -r "${PEARL_PKGDIR}/powerline" "$HOME/.config/"
+        info "Set powerline configuration successfully"
+    fi
+
     return 0
 }
 
@@ -18,5 +27,6 @@ function post_update(){
 
 function pre_remove(){
     rm -rf "${PEARL_PKGVARDIR}/plugins"
+    [[ -e $HOME/.config/powerline ]] && rm -rf "$HOME/.config/powerline"
     return 0
 }
